562955d87c3f8c1e552337a6620ed6a77f06ab4d
mcastroa7 <manucastroa7@gmail.com>
2026-01-15 13:27:36 -0300

    cambiando login

--- components/Login.tsx
+++ components/Login.tsx
@@ -42,7 +42,7 @@ const Login: React.FC = () => {
         setError('');
 
         try {
-            const res = await fetch(`${API_BASE_URL}/api/auth/login`, {
+            const res = await fetch(`${API_BASE_URL}/auth/login`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, password }),
